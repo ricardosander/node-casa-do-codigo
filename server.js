a66fb84ea306c7483dc7ccc92910c48c25dbfd81
Ricardo Sander Lopes <ricardo.lopes@ifood.com>
2020-11-11 11:19:05 -0300

    Adicionando log mais detalhado a requisição.

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const http = require('http');
 
 const server = http.createServer(function(request, response) {
-    console.log('Recieving request');
+    
+    console.log(`Recieving request ${request.method} - ${request.url}`);
+    
     response.end(`
         <html>
             <head>
