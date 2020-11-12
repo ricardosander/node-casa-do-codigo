0ce1a2f5299d3ef2cd9eb1559f0d5071411fbd01
Ricardo Sander Lopes <ricardo.lopes@ifood.com>
2020-11-12 18:15:33 -0300

    usando Marko template com Express.

--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -1,7 +1,6 @@
 module.exports = (app) => {
     
     app.get('/', (request, response) => {
-        console.log(`Recieving request ${request.method} - ${request.url}`);
         
         response.end(`
             <html>
@@ -15,16 +14,9 @@ module.exports = (app) => {
     });
     
     app.get('/livros', (request, response) => {
-        console.log(`Recieving request ${request.method} - ${request.url}`);
         
-        response.end(`
-            <html>
-                <head>
-                    <meta charset="utf-8">
-                </head>
-                <body>
-                    <h1>Listagem de Livros</h1>
-                </body>
-            </html>`);
+        response.marko(
+            require('../views/livros/lista/lista.marko')
+        );
     });
 }
--- src/config/custom-express.js
+++ src/config/custom-express.js
@@ -1,7 +1,10 @@
+require('marko/node-require').install();
+require('marko/express');
+
 const express = require('express');
 const app = express();
 
-const rotas = require('../app/rotas');
+const rotas = require('../app/rotas/rotas');
 rotas(app);
 
 module.exports = app;
